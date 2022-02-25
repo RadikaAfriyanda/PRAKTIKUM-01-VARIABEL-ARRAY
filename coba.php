d3a9ae16f05f490dafbb0106658f627da686607b
Radika Afriyanda <92706634+RadikaAfriyanda@users.noreply.github.com>
2022-02-25 11:23:21 +0700

    Add files via upload

---
+++ coba.php
@@ -0,0 +1,10 @@
+<?php
+$nama = "radika";
+// contoh menulis variable menggunakan underscore
+$jenis_kelamin = "perempuan";
+// contoh menulis variable menggunakan comelcase
+$alamatsaya = "depok";
+echo "Nama saya $nama ";
+echo "<br> Jenis kelamin $jenis_kelamin ";
+echo "<br> Alamat : $alamatsaya ";
+?>
---
+++ variable-user.php
@@ -0,0 +1,12 @@
+<?php
+// definisikan variable
+$nama = "Radika Afriyanda";
+$umur = 18;
+$berat = 50.5;
+
+echo "Nama : $nama ";
+echo "<br>Umur : $umur Tahun ";
+echo "<br>Berat : $berat kg ";
+
+echo "<br> Hello $nama Apa kabar";
+?>
